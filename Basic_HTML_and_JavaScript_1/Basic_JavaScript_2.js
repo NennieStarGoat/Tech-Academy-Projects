3989a34e1c4fb45f9d64088551684e0a0ccbd4c0
NennieStarGoat <jennieamsmith@gmail.com>
2025-04-29 16:38:45 -0700

    continuing work on the coding bootcamp
    updated various files to continue learning

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -1,5 +1,5 @@
 function My_First_Function() {
-	var String = "You pet the goat!";
+	var String = "You pet the goat! <p><b>Congratulations!</b> <p>The goat, <i>Imogen</i>, is <u>very happy</u>.";
 	var result = String.fontcolor("green");
 	document.getElementById("Goat").innerHTML =
 	result;

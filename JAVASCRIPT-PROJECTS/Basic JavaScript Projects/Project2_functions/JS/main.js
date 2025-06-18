86b144d2b718dbf17410ad165c7584f0383fcdd1
NennieStarGoat <jennieamsmith@gmail.com>
2025-06-18 15:43:33 -0700

    more change

--- JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project2_functions/JS/main.js
+++ JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,6 @@
 function Second_Function() { //Initializes a function that turns a button's text once clicked on within the html document.
     var str = "This is the button text!", str2 = " You've done it!";
     document.getElementById("Button_Text").innerHTML = str + str2;
-}
 
-document.getElementById("Paragraph") += " But surprise! There is more!";
+    document.getElementById("Paragraph").innerHTML += " But surprise! There is more!"; //Concatenates and adds onto the Paragraph element in the html document.
+}

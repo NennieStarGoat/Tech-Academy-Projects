b1dd1f5640f4d7e58401acd9820f9dce7d3ae0fa
NennieStarGoat <jennieamsmith@gmail.com>
2025-06-18 13:59:28 -0700

    new JS is here

---
+++ JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+document.write("Hello World!  "); //Writes Hello World! At the top of an html document.
+
+var A = "This is a string"; //Initializes a variable A with the value "This is a string".
+
+document.write(A); //Writes the above variable.
+
+var B = "This is an initial pop up message string, this time with \'Quotation marks\'."; //Initializes a variable B with the value "This is an initial pop up message string, this time with 'Quotation marks'.".
+
+window.alert(B); //Creates a pop up alert on opening the html document, displaying the above.
+
+var C = "  And now this is another string" + " and another string added. "; //Initializes another variable and concatenates both sets of strings together into one.
+
+document.write(C); //Writes the above variable.
+
+var D = "This is D. ", E = "This is E. ", F = "This is F. "; //Initializes multiple variables within the same block of code, any of them may be called.
+
+document.write(F); //Writes the above variable F specifically.
+
+document.write(3 * 3); //Writes the result of the mathematical operation, 9.
+
+function My_First_Function() { //Initializes a function that turns a button's text once clicked on within the html document.
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
